33ac7c315af53246a589ca9359ab90796f6bfafa
machamp0714 <machamp0714@gmail.com>
2022-07-16 23:07:57 +0900

    create: LoginForm

---
+++ stories/LoginForm.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { within, userEvent } from '@storybook/testing-library';
+// import { expect } from '@storybook/jest';
+
+import { LoginForm } from './LoginForm';
+
+export default {
+  title: 'Components/LoginForm',
+  component: LoginForm
+} as ComponentMeta<typeof LoginForm>
+
+const Template: ComponentStory<typeof LoginForm> = (_args) => {
+  return <LoginForm />
+}
+
+export const EmptyForm = Template.bind({});
+
+export const FilledForm = Template.bind({});
+FilledForm.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await userEvent.type(canvas.getByLabelText('email'), 'machamp0714@gmail.com');
+  await userEvent.type(canvas.getByLabelText('password'), 'machamp');
+
+  await userEvent.click(canvas.getByRole('button'));
+}
---
+++ stories/LoginForm.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+export const LoginForm: React.FC = () => {
+  const [values, setValues] = useState({ email: '', password: '' })
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('ok');
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const newValues = {
+      ...values,
+      [name]: event.target.value
+    }
+    setValues(newValues);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-6">
+        <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>email</label>
+        <input 
+          type='text'
+          name='email'
+          value={values.email}
+          id='email'
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-6">
+        <label htmlFor='password'>password</label>
+        <input 
+          type='password'
+          name='password'
+          value={values.password}
+          id='password'
+          onChange={handleChange}
+        />
+      </div>
+      <button type='submit' className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Submit</button>
+    </form>
+  )
+}
