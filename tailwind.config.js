eec11c238ce8b54a64a66372a4dae8c16c9cbc04
machamp0714 <machamp0714@gmail.com>
2022-07-17 22:45:37 +0900

    ref: tailwindcss in react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,5 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
   content: [
-    "./app/javascript/src/**/*.{js,jsx,ts,tsx}",
+    './app/javascript/src/**/*.{js,jsx,ts,tsx}'
   ],
 }
