0991b7b638f7470d8ffecf88ddb8944342418eef
machamp0714 <machamp0714@gmail.com>
2022-08-06 20:23:45 +0900

    add: create action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,3 +1,18 @@
 class BlogsController < ApplicationController
   def new; end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      render json: @blog, status: :ok
+    else
+      render json: @blog.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
 end
