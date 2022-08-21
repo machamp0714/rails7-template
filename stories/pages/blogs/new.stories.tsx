88b703410fadd7abd66d32806a8f7160277702dd
machamp0714 <machamp0714@gmail.com>
2022-08-21 21:17:24 +0900

    modify: use MSW on create blog

--- stories/pages/blogs/new.stories.tsx
+++ stories/pages/blogs/new.stories.tsx
@@ -1,5 +1,6 @@
 import { ComponentMeta } from '@storybook/react';
 import { userEvent, screen } from '@storybook/testing-library';
+import { rest } from 'msw';
 
 import { BlogsNew } from '../../../app/javascript/src/pages/blogs/new';
 
@@ -7,7 +8,26 @@ export default {
   component: BlogsNew,
 } as ComponentMeta<typeof BlogsNew>;
 
-export const Empty = {};
+export const Empty = {
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post('/blogs', (_req, res, ctx) => {
+          return res(
+            ctx.status(200),
+            ctx.json({
+              id: 1,
+              title: 'title',
+              description: 'description',
+              created_at: '2020-01-01',
+              updated_at: '2020-01-01',
+            })
+          );
+        }),
+      ],
+    },
+  },
+};
 
 export const EmptyError = {
   ...Empty,
