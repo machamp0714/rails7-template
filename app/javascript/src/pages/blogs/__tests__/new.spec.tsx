3b2f7a39fe429c827f635315e680a048317b8cd5
machamp0714 <machamp0714@gmail.com>
2022-10-29 13:18:44 +0900

    add: validation tests

--- app/javascript/src/pages/blogs/__tests__/new.spec.tsx
+++ app/javascript/src/pages/blogs/__tests__/new.spec.tsx
@@ -5,8 +5,14 @@ import '@testing-library/jest-dom';
 import { setup } from '../../../test/test-utils';
 import { BlogsNew } from '../new';
 
-test('rendering', () => {
-  setup(<BlogsNew blogs={[]} />);
+describe('BlogsNew', () => {
+  describe('タイトルが空白の時', () => {
+    it('バリデーションメッセージが表示される', async () => {
+      const { user } = setup(<BlogsNew blogs={[]} />);
 
-  expect(screen.getByText('ブログ作成')).toBeInTheDocument();
+      user.click(screen.getByRole('button', { name: 'Post' }));
+
+      expect(await screen.findByText('Title is required')).toBeInTheDocument();
+    });
+  });
 });
