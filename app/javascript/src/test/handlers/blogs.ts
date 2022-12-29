9ea64102e9568cbd8673739d5b2e6bc0317a140d
machamp0714 <machamp0714@gmail.com>
2022-12-29 22:45:15 +0900

    add: faker

--- app/javascript/src/test/handlers/blogs.ts
+++ app/javascript/src/test/handlers/blogs.ts
@@ -1,41 +1,38 @@
 import { rest } from 'msw';
+import { faker } from '@faker-js/faker/locale/ja';
 
 export const handlers = {
-  index: [
-    rest.get('/blogs', (_req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json([
-          {
-            id: 1,
-            title: 'title',
-            description: 'description',
-            created_at: '2020-01-01',
-            updated_at: '2020-01-01',
-          },
-          {
-            id: 2,
-            title: 'title2',
-            description: 'description',
-            created_at: '2020-01-01',
-            updated_at: '2020-01-01',
-          },
-        ])
-      );
-    }),
-  ],
-  create: [
-    rest.post('/blogs', (_req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          id: 1,
-          title: 'title',
-          description: 'description',
+  index: rest.get('/blogs', (_req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: faker.random.numeric(),
+          title: faker.lorem.word(),
+          description: faker.lorem.sentence(),
           created_at: '2020-01-01',
           updated_at: '2020-01-01',
-        })
-      );
-    }),
-  ],
+        },
+        {
+          id: faker.random.numeric(),
+          title: faker.lorem.word(),
+          description: faker.lorem.sentence(),
+          created_at: '2020-01-01',
+          updated_at: '2020-01-01',
+        },
+      ])
+    );
+  }),
+  create: rest.post('/blogs', (_req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        id: faker.random.numeric(),
+        title: faker.lorem.word(),
+        description: faker.lorem.sentence(),
+        created_at: '2020-01-01',
+        updated_at: '2020-01-01',
+      })
+    );
+  }),
 };
