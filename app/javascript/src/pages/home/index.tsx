d45a42713e6258d1cefcae42a48fda1cf7ee2514
machamp0714 <machamp0714@gmail.com>
2023-01-09 14:15:38 +0900

    forwardRef

--- app/javascript/src/pages/home/index.tsx
+++ app/javascript/src/pages/home/index.tsx
@@ -1,64 +1,20 @@
-import React, { useRef } from 'react';
+import React, { useRef, forwardRef } from 'react';
+
+const MyInput = forwardRef<HTMLInputElement>(function MyInput(props, ref) {
+  return <input {...props} ref={ref} />;
+});
 
 export const Home = () => {
-  const itemsRef = useRef<Map<number, HTMLLIElement>>(null!);
+  const inputRef = useRef<HTMLInputElement>(null!);
 
-  const scrollToId = (id: number) => {
-    const map = getMap();
-    const node = map.get(id);
-    node?.scrollIntoView({
-      behavior: 'smooth',
-      block: 'nearest',
-      inline: 'center',
-    });
-  };
-
-  const getMap = () => {
-    if (!itemsRef.current) {
-      itemsRef.current = new Map();
-    }
-    return itemsRef.current;
-  };
+  function handleClick() {
+    inputRef.current.focus();
+  }
 
   return (
     <>
-      <nav>
-        <button onClick={() => scrollToId(0)}>Tom</button>
-        <button onClick={() => scrollToId(5)}>Maru</button>
-        <button onClick={() => scrollToId(9)}>Jellylorum</button>
-      </nav>
-      <div>
-        <ul>
-          {catList.map((cat) => (
-            <li
-              key={cat.id}
-              ref={(node) => {
-                const map = getMap();
-                if (node) {
-                  map.set(cat.id, node);
-                } else {
-                  map.delete(cat.id);
-                }
-              }}
-            >
-              <img src={cat.imageUrl} alt={`Cat #${cat.id}`} />
-            </li>
-          ))}
-        </ul>
-      </div>
+      <MyInput ref={inputRef} />
+      <button onClick={handleClick}>Focus the input</button>
     </>
   );
 };
-
-interface Cat {
-  id: number;
-  imageUrl: string;
-}
-
-const catList: Array<Cat> = [];
-for (let i = 0; i < 10; i++) {
-  catList.push({
-    id: i,
-    imageUrl: `https://placekitten.com/250/200?image=${i}`,
-  });
-}
