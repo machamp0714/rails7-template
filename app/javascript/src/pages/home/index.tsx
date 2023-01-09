de69a9c5bdc1922e0ac6a19dc2aef767027422b8
machamp0714 <machamp0714@gmail.com>
2023-01-09 13:48:35 +0900

    useRef

--- app/javascript/src/pages/home/index.tsx
+++ app/javascript/src/pages/home/index.tsx
@@ -1,26 +1,64 @@
-import React, { useEffect } from 'react';
-
-export function createConnection() {
-  // A real implementation would actually connect to the server
-  return {
-    connect() {
-      console.log('✅ Connecting...');
-    },
-    disconnect() {
-      console.log('❌ Disconnected.');
-    },
-  };
-}
+import React, { useRef } from 'react';
 
 export const Home = () => {
-  useEffect(() => {
-    const connection = createConnection();
-    connection.connect();
+  const itemsRef = useRef<Map<number, HTMLLIElement>>(null!);
 
-    return () => {
-      connection.disconnect();
-    };
-  });
+  const scrollToId = (id: number) => {
+    const map = getMap();
+    const node = map.get(id);
+    node?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center',
+    });
+  };
 
-  return <h1>Welcome to the chat!</h1>;
+  const getMap = () => {
+    if (!itemsRef.current) {
+      itemsRef.current = new Map();
+    }
+    return itemsRef.current;
+  };
+
+  return (
+    <>
+      <nav>
+        <button onClick={() => scrollToId(0)}>Tom</button>
+        <button onClick={() => scrollToId(5)}>Maru</button>
+        <button onClick={() => scrollToId(9)}>Jellylorum</button>
+      </nav>
+      <div>
+        <ul>
+          {catList.map((cat) => (
+            <li
+              key={cat.id}
+              ref={(node) => {
+                const map = getMap();
+                if (node) {
+                  map.set(cat.id, node);
+                } else {
+                  map.delete(cat.id);
+                }
+              }}
+            >
+              <img src={cat.imageUrl} alt={`Cat #${cat.id}`} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
+
+interface Cat {
+  id: number;
+  imageUrl: string;
+}
+
+const catList: Array<Cat> = [];
+for (let i = 0; i < 10; i++) {
+  catList.push({
+    id: i,
+    imageUrl: `https://placekitten.com/250/200?image=${i}`,
+  });
+}
